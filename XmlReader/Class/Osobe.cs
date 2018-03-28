26ea0888746a9a7b8fc964eb97bc09ccf129783e
Dusan <dusan-nikolic88@hotmail.com>
2018-03-28 19:06:08 +0200

    Xml_Reader
    Read and Expot XML to CSV format

---
+++ XmlReader/Class/Osobe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlCitac.Class
+{
+    public class Osobe
+    {
+        public string email { get; set; }
+        public string prezimeime { get; set; }
+        public string adresa { get; set; }
+        public string datumrodjenja { get; set; }
+        public string brojtelefona { get; set; }
+    }
+}
---
+++ XmlReader/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using XmlCitac.Class;
+
+namespace XmlCitac
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Osobe> lista = new List<Osobe>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+           
+
+            XmlTextReader reader = new XmlTextReader("C:/listaemail.xml");
+
+            while (reader.Read())
+            {
+                var a = new Osobe();
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+
+                        while (reader.MoveToNextAttribute())
+                        {
+                            if (reader.Name == "email")
+                            {
+                               
+                                a.email = reader.Value;
+                                lista.Add(a);                             
+                            }
+                            if (reader.Name == "prezimeime")
+                            {
+
+                                a.prezimeime = reader.Value;
+                                lista.Add(a);
+                            }
+
+                            if (reader.Name == "adresa")
+                            {
+
+                                a.adresa = reader.Value;
+                                lista.Add(a);
+                            }
+                            if (reader.Name == "datumrodjenja")
+                            {
+
+                                a.datumrodjenja = reader.Value;
+                                lista.Add(a);
+                            }
+                            if (reader.Name == "brojtelefona")
+                            {
+
+                                a.brojtelefona = reader.Value;
+                                lista.Add(a);
+                            }
+                        }
+
+                        break;
+                }
+                
+            }
+
+            foreach (var item in lista.Distinct())
+            {
+                dataGridClanoviIgraci.Items.Add(item);
+            }
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            dataGridClanoviIgraci.SelectAllCells();
+            dataGridClanoviIgraci.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+            ApplicationCommands.Copy.Execute(null, dataGridClanoviIgraci);
+            dataGridClanoviIgraci.UnselectAllCells();
+            String result = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
+            File.AppendAllText("C:\\lista.csv", result, Encoding.UTF8);
+        }
+    }
+}
